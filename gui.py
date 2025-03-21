8958667f9be225f758076541feef61221dd40dfe
Alexander Silerio <mr.alexandersc@outlook.com>
2025-03-21 09:10:55 -0500

    changed to (mostly) dark mode, need more changes in GUI

--- gui.py
+++ gui.py
@@ -5,20 +5,24 @@ class GraphVisualizerGUI(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        self.master.configure(bg="#2f2f2f")
+        self.pack(pady=20, padx=10, fill="both", expand=True)
         self.create_widgets()
         self.graph_ops = GraphOperations()
 
     def create_widgets(self):
-        tk.Label(self, text="Enter nodes (comma-separated):").pack(side=tk.LEFT)
-        self.node_entry = tk.Entry(self)
-        self.node_entry.pack(side=tk.LEFT)
+        self.node_label = tk.Label(self, text="Enter nodes (comma-separated):", bg="#2f2f2f", fg="white")
+        self.node_label.pack(side=tk.LEFT, padx=10)
+        self.node_entry = tk.Entry(self, bg="#4f4f4f", fg="white")
+        self.node_entry.pack(side=tk.LEFT, padx=10)
 
-        tk.Label(self, text="Enter edges (node1-node2, comma-separated):").pack(side=tk.LEFT)
-        self.edge_entry = tk.Entry(self)
-        self.edge_entry.pack(side=tk.LEFT)
+        self.edge_label = tk.Label(self, text="Enter edges (node1-node2, comma-separated):", bg="#2f2f2f", fg="white")  
+        self.edge_label.pack(side=tk.LEFT, padx=10)
+        self.edge_entry = tk.Entry(self, bg="#4f4f4f", fg="white")
+        self.edge_entry.pack(side=tk.LEFT, padx=10)
 
-        tk.Button(self, text="Visualizer", command = self.visualize_graph).pack(side=tk.LEFT)
+        self.visualize_button = tk.Button(self, text="Visualizer", command = self.visualize_graph, bg="#4f4f4f", fg="white")
+        self.visualize_button.pack(side=tk.LEFT, padx=10)
 
     def visualize_graph(self):
         nodes = self.node_entry.get().split(',')
